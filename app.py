25aa7f0f2bba0ea3f734bc500ac913c834017e6c
Ar-jun-fs9 <arjunbhul5799@gmail.com>
2026-02-08 11:48:57 +0545

    deployed successfully

--- app.py
+++ app.py
@@ -232,9 +232,13 @@ def generate_summary():
         return jsonify({"error": str(e)}), 500
 
 
+# uncomment for local testing
+
 # if __name__ == "__main__":
 #     app.run()
 
+
+# comment for local testing
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
